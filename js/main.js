934fbc29b8c96baaa15b6397cfb038fc3c308379
Nick Muravev <67981036+muravevnv@users.noreply.github.com>
2023-07-14 14:14:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let drodownToggle = $('.dropdown-toggle');
+
+drodownToggle.on('click', function(){
+  let drodown = $(this).closest('.dropdown');
+  drodown.toggleClass('show')
+})
+
+
+let subElement = $('.has-sub');
+
+subElement.on('click', function(){
+  let subMenu = $(this).find('.submenu');
+
+  $(this).toggleClass('open');
+  subMenu.slideToggle();
+})
+
+
+$('.sidebar-toggler').on('click', function(){
+  $sidebar = $(this).closest('.sidebar');
+  $mainContent = $('.main-content');
+
+  $sidebar.toggleClass('open');
+  $mainContent.toggleClass('open');
+  $(this).toggleClass('open');
+
+  subElement.find('.submenu').slideUp();
+  
+})
+
+$('.header-burger').on('click', function(){
+  $('.sidebar').addClass('active')
+})
+
+$('.sidebar-close').on('click', function(){
+  $('.sidebar').removeClass('active');
+})
+
+
+
+$('.js-open-modal').on('click', function(){
+  $('.modal-overlay').fadeIn();
+  var id = $(this).data('modal');
+  $('.modal[data-modal="' + id + '"]').fadeIn();
+});
+
+$('.modal-close').on('click', function(){
+  $('modal-overlay').fadeOut();
+  $('.modal').fadeOut();
+});
+$('.modal-overlay').on('click', function(){
+  $('modal-overlay').fadeOut();
+  $('.modal').fadeOut();
+});
